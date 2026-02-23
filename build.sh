405bfc84f77af5a25a16c3dd4664ab0763f9f2ab
Faelis <faelis@gmail.com>
2026-02-23 09:18:17 +0000

    chore: add trimpath to build command

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ if [ $? -ne 0 ]; then
     echo "tests failed. Exiting."
     exit 1
 fi
-go build -o goweasel -ldflags "-s -w" main.go
+go build -o goweasel -trimpath -ldflags "-s -w" main.go
 upx --ultra-brute goweasel
